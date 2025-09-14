8a0079e08ef55a71aa5728a6a5e0d47d24128957
Saksham-verma28 <saksham1449.be23@chitkarauniversity.edu.in>
2025-09-14 13:46:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
 
+import React from 'react';
+import { Navigation } from './components/Navigation';
+import { DemoContent } from './components/DemoContent';
+import { FloatingElements } from './components/FloatingElements';
+import { LoadingAnimation } from './components/LoadingAnimation';
+
+export default function App() {
+  return (
+    <div className="relative">
+      <LoadingAnimation />
+      <Navigation />
+      <DemoContent />
+      <FloatingElements />
+    </div>
+  );
+}
